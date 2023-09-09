a929568cdc5ee2bcd6d35c6b4cf0295c00fdecb2
umair <umairghani156@gamil.com>
2023-09-09 18:03:37 +0500

    Javascript Assignment 3

---
+++ app3.js
@@ -0,0 +1,23 @@
+// Question 1
+var age;
+age="I am 21 years old";
+alert(age)
+
+// Question 2
+var times;
+times="You have visited this site 14 times"
+alert(times)
+
+// Question 3
+var birthyear;
+birthyear="My Birth year is 2002"
+document.write(birthyear+ "\n Data type of my declared is number<br>")
+
+// Question 4
+var visitorName;
+ visitorName="Akbar"
+var product;
+product="T-shirt(s)"
+var quantity;
+quantity= 5;
+document.write(visitorName+" ordered "+quantity+" "+product+" on XYZ Clothing store")
